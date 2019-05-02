6d6d4c5db73e74bc1083550d9168672501dc0e2e
Nico Ahola <nico.ahola@student.tut.fi>
2019-05-02 20:58:31 +0300

    Initial commit

---
+++ src/main/java/com/turvohj/htyo/turvohjhtyo/TurvohjHtyoApplication.java
@@ -0,0 +1,13 @@
+package com.turvohj.htyo.turvohjhtyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurvohjHtyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurvohjHtyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turvohj/htyo/turvohjhtyo/TurvohjHtyoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.turvohj.htyo.turvohjhtyo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TurvohjHtyoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
